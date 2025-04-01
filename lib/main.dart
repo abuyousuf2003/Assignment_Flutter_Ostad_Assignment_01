31f1d46e12969ec39c4817fa8cdf6c010a4cb1cb
Abu Yousuf <abuyousufmdjumman@gmail.com>
2025-04-01 16:15:44 +0600

    Updated code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
-///hellooobxnbadhahcb
+abstract class Vehicles{
+double _speed;
+Vehicles(this._speed);
+ void move();
+set setSpeed(double speed)
+ {
+_speed = speed;
+ }
+
+
+}
+class Car extends Vehicles{
+  Car(double speed):super(speed);
+
+  @override
+ void move()
+ {
+print("The car is moving at ${_speed} km/h");
+ }
+}
+
+
+void main()
+{
+Car car = Car(0.00);
+car.setSpeed=120;
+car.move();
+}
